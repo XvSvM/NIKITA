651a983ae0530a388fd7a2d0d8c9183c6cc2eba8
XvSvM <vadiagadia@gmail.com>
2024-12-05 21:09:27 +0300

    Update twrp_ampere.mk

--- twrp_ampere.mk
+++ twrp_ampere.mk
@@ -11,8 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
-
 
 # Inherit from ampere device
 $(call inherit-product, device/amlogic/ampere/device.mk)
