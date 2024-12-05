2fb058ebac787a2bfbba6d19c6edfa3e10172daa
XvSvM <vadiagadia@gmail.com>
2024-12-05 19:45:45 +0300

    Update twrp_ampere.mk

--- twrp_ampere.mk
+++ twrp_ampere.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit some common Twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
