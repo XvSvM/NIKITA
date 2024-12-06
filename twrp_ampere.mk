28c15276502b0f54d2b879eda923fdc4e716f2e8
XvSvM <vadiagadia@gmail.com>
2024-12-06 22:59:04 +0300

    Update twrp_ampere.mk

--- twrp_ampere.mk
+++ twrp_ampere.mk
@@ -10,7 +10,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
