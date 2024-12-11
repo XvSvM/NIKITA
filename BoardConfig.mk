816fd8c924fe15ab4b4e908fcfde9a2b24b7bb9c
XvSvM <vadiagadia@gmail.com>
2024-12-11 16:33:51 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a9
+TARGET_CPU_VARIANT := generetic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a9
 
 TARGET_USES_64_BIT_BINDER := true
